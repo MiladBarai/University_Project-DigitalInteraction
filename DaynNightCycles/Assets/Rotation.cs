f1d3f87acf348c2bd09be0f7ae1f00ebb2f2f96e
batorai <miladbarai@gmail.com>
2016-08-05 17:16:45 +0200

    Island Simple Changes
    Not much so far ~~

--- DaynNightCycles/Assets/Rotation.cs
+++ DaynNightCycles/Assets/Rotation.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotation : MonoBehaviour {
-
+public class Rotation : MonoBehaviour
+{
     float v;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start ()
+    {
 
-    public void Set(float velocity) {
+    }
+
+    public void Set(float velocity)
+    {
         v = velocity;
     }
+
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
         transform.RotateAround(Vector3.zero, Vector3.right, v * Time.deltaTime);
         transform.LookAt(Vector3.zero);
-	}
+    }
 }
