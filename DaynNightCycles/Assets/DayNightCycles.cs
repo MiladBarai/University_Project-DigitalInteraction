79aa5770f2da4e56cd841cd6eb11c7bf9e639327
chrillejb <chrille.jb@gmail.com>
2016-08-30 16:05:57 +0200

    Merge
    Merged skyboxes with terrain

---
+++ DaynNightCycles/Assets/DayNightCycles.cs
@@ -0,0 +1,281 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Clock
+{
+    public int hour;
+    public int min;
+
+    public Clock(int h, int m)
+    {
+        hour = h;
+        min = m;
+    }
+}
+
+public class DayNightCycles : MonoBehaviour
+{
+
+    public int secondsInADay = 24;
+    public bool showGUI = true;
+    public Clock gameClock = new Clock(8, 0);
+
+    GameObject SunLight;
+    GameObject MoonLight;
+
+    private float clockValue;
+    private int hour;
+    private int min;
+    private float timeMult;
+
+    private bool isDay = false;
+    private bool isDusk = false;
+    private bool isNight = false;
+    private bool isDawn = false;
+
+    private Material skyboxDayToDusk;
+    private Material skyboxDuskToNight;
+    private Material skyboxNightToDawn;
+    private Material skyboxDawnToDay;
+
+    private float rot = 0;
+    private float blend = 0;
+
+    private Light Suncomp;
+    private Light Mooncomp;
+    private GameObject Moon;
+    private GameObject Sun;
+
+    private float sunIntensity = 1.5f;
+    private float moonIntensity = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        hour = gameClock.hour % 24;
+        min = gameClock.min % 60;
+        clockValue = (hour * 3600) + (min * 60);
+
+        //In case the seconds in a day was not specified
+        if (secondsInADay < 24)
+        {
+            secondsInADay = 24;
+        }
+        timeMult = 86400 / secondsInADay;
+
+        SunLight = new GameObject("Sunlight");
+        MoonLight = new GameObject("Moonlight");
+
+        Suncomp = SunLight.AddComponent<Light>();
+        Mooncomp = MoonLight.AddComponent<Light>();
+
+        // Changing type to directional
+        Suncomp.type = LightType.Directional;
+        Mooncomp.type = LightType.Directional;
+
+        Color S = new Color();
+        Color M = new Color();
+
+        // Hex to RGB
+        ColorUtility.TryParseHtmlString("#ff9933", out S);
+        ColorUtility.TryParseHtmlString("#e6ffff", out M);
+        Suncomp.color = S;
+        Mooncomp.color = M;
+
+        // Setting intensity
+        Suncomp.intensity = sunIntensity;
+        Mooncomp.intensity = moonIntensity;
+
+        //CREATING MOON -------------------------------------------------------------- Start
+        Moon = (GameObject)Instantiate(Resources.Load("MoonSphere"));
+        Moon.name = "Moon";
+
+        //Size
+        Moon.transform.localScale = new Vector3(50, 50, 50);
+
+        //Setting Material on Moon
+        Material mat = Resources.Load("W132", typeof(Material)) as Material;
+        MeshRenderer r = Moon.GetComponent<MeshRenderer>();
+
+        // Setting Material Color
+        mat.color = Color.white;
+        r.material = mat;
+
+        //Works
+        Moon.transform.parent = MoonLight.transform;
+        //CREATING MOON -------------------------------------------------------------- End
+
+        // CREATING SUN -------------------------------------------------------------- Start
+        // Using a Prefab sphere with a halo
+        GameObject Sun = (GameObject)Instantiate(Resources.Load("SunSphere"));
+        Sun.name = "Sun";
+
+        // Size
+        Sun.transform.localScale = new Vector3(25, 25, 25);
+
+        // Setting Material on Sun
+        mat = Resources.Load("SunMat", typeof(Material)) as Material;
+        r = Sun.GetComponent<MeshRenderer>();
+
+        // Setting Material Color
+        mat.color = S;
+        r.material = mat;
+        Sun.transform.parent = SunLight.transform;
+        // CREATING SUN -------------------------------------------------------------- End
+
+        skyboxDayToDusk = Resources.Load("Skyboxes/SkyboxDayToDusk") as Material;
+        skyboxDuskToNight = Resources.Load("Skyboxes/SkyboxDuskToNight") as Material;
+        skyboxNightToDawn = Resources.Load("Skyboxes/SkyboxNightToDawn") as Material;
+        skyboxDawnToDay = Resources.Load("Skyboxes/SkyboxDawnToDay") as Material;
+
+        if (skyboxDayToDusk != null && skyboxDuskToNight != null
+            && skyboxNightToDawn != null && skyboxDawnToDay != null)
+        {
+            Debug.Log("All skuboxes loaded peoperly.");
+        }
+        else
+        {
+            Debug.Log("A skybox couldn't load.");
+        }
+    }
+
+    //y and z coordinates for sun and moon
+    private float y;
+    private float z;
+    //rotation multipliers
+    private float dayMult = Mathf.PI / 57600;
+    private float nightMult = Mathf.PI / 28800;
+    private float distance = 800f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour = ((int)clockValue / 3600) % 24;
+        min = ((int)clockValue / 60) % 60;
+
+        if (hour >= 20 && hour < 22)
+        {
+            isDay = false; isDusk = true;
+        }
+        else if (hour >= 8 && hour < 20)
+        {
+            isDay = true; isDawn = false;
+        }
+        else if (hour >= 6 && hour < 8)
+        {
+            isNight = false; isDawn = true;
+        }
+        else//if (hour >= 22 || (hour >= 0 && hour < 6))
+        {
+            isDusk = false; isNight = true;
+        }
+
+        if (isDawn || isDay || isDusk)
+        {
+            //MoonLight.transform.position = new Vector3(0, -100, 0);
+            //MoonComp.intensity = 0;
+            MoonLight.SetActive(false);
+            SunLight.SetActive(true);
+
+            y = Mathf.Sin(dayMult * (clockValue - 21600)) * distance;
+            z = Mathf.Cos(dayMult * (clockValue - 21600)) * distance;
+            SunLight.transform.position = new Vector3(0, y, -z);
+            SunLight.transform.LookAt(Vector3.zero);
+            Suncomp.intensity = sunIntensity;
+        }
+        else
+        {
+            /*SunLight.transform.position = new Vector3(0, -100, 0);
+            Suncomp.intensity = 0;*/
+            SunLight.SetActive(false);
+            MoonLight.SetActive(true);
+            if (hour >= 22)
+            {
+                y = Mathf.Sin(nightMult * (clockValue - 79200)) * distance;
+                z = Mathf.Cos(nightMult * (clockValue - 79200)) * distance;
+            }
+            else
+            {
+                y = Mathf.Sin(nightMult * (clockValue + 7200)) * distance;
+                z = Mathf.Cos(nightMult * (clockValue + 7200)) * distance;
+            }
+            MoonLight.transform.position = new Vector3(0, y, -z);
+            MoonLight.transform.LookAt(Vector3.zero);
+            Mooncomp.intensity = moonIntensity;
+        }
+
+        clockValue += (Time.deltaTime * timeMult);
+        clockValue = clockValue % 86400;
+
+        float gameSecond = secondsInADay / 86400f; // A gamesecond
+
+        float fifteenMin = 900 * gameSecond;
+
+        float blendConst = 1 / fifteenMin;
+
+        if (isDusk)
+        {
+            if (blend < 1 && RenderSettings.skybox.name == "SkyboxDayToDusk")
+                blend += (blendConst * Time.deltaTime);
+            else
+            {
+                RenderSettings.skybox = skyboxDuskToNight;
+                blend = 0f;
+            }
+        }
+        if (isNight)
+        {
+            if (blend < 1 && RenderSettings.skybox.name == "SkyboxDuskToNight")
+                blend += (blendConst * Time.deltaTime);
+            else
+            {
+                RenderSettings.skybox = skyboxNightToDawn;
+                blend = 0f;
+            }
+        }
+        if (isDawn)
+        {
+            if (blend < 1 && RenderSettings.skybox.name == "SkyboxNightToDawn")
+                blend += (blendConst * Time.deltaTime);
+            else
+            {
+                RenderSettings.skybox = skyboxDawnToDay;
+                blend = 0f;
+            }
+        }
+        if (isDay)
+        {
+            if (blend < 1 && RenderSettings.skybox.name == "SkyboxDawnToDay")
+                blend += (blendConst * Time.deltaTime);
+            else
+            {
+                RenderSettings.skybox = skyboxDayToDusk;
+                blend = 0f;
+            }
+        }
+
+        RenderSettings.skybox.SetFloat("_Blend", blend);
+
+        rot += 0.01f;
+        rot = rot % 360;
+        RenderSettings.skybox.SetFloat("_Rotation", rot);
+    }
+
+    private GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        if (showGUI)
+        {
+            guiStyle.normal.textColor = Color.red;
+            guiStyle.fontSize = 30;
+            string hourS = hour.ToString().PadLeft(2, '0');
+            string minS = min.ToString().PadLeft(2, '0');
+            GUILayout.Label(hourS + ":" + minS, guiStyle);
+            if (isDay) GUILayout.Label("Day", guiStyle);
+            if (isDusk) GUILayout.Label("Dusk", guiStyle);
+            if (isNight) GUILayout.Label("Night", guiStyle);
+            if (isDawn) GUILayout.Label("Dawn", guiStyle);
+        }
+    }
+}
---
+++ DaynNightCycles/Assets/Resources/Shaders/SkyboxBlended.shader
@@ -0,0 +1,189 @@
+﻿Shader "Skybox/Blended 6 Sided" 
+{
+	Properties
+	{
+		
+		_Blend("Blend", Range(0.0, 1.0)) = 0.5		
+		_Rotation("Rotation", Range(0, 360)) = 0
+		[NoScaleOffset] _FrontTex("Front [+Z] (1:st)", 2D) = "white" {}
+		[NoScaleOffset] _BackTex("Back [-Z] (1:st)", 2D) = "white" {}
+		[NoScaleOffset] _LeftTex("Left [+X] (1:st)", 2D) = "white" {}
+		[NoScaleOffset] _RightTex("Right [-X] (1:st)", 2D) = "white" {}
+		[NoScaleOffset] _UpTex("Up [+Y] (1:st)", 2D) = "white" {}
+		[NoScaleOffset] _DownTex("Down [-Y] (1:st)", 2D) = "white" {}
+		_Tint1("Tint Color (1:st)", Color) = (.5, .5, .5, .5)
+		[Gamma] _Exposure1("Exposure (1:st)", Range(0, 8)) = 1.0
+		[NoScaleOffset] _FrontTex2("Front [+Z] (2:nd)", 2D) = "white" {}
+		[NoScaleOffset] _BackTex2("Back [-Z] (2:nd)", 2D) = "white" {}
+		[NoScaleOffset] _LeftTex2("Left [+X] (2:nd)", 2D) = "white" {}
+		[NoScaleOffset] _RightTex2("Right [-X] (2:nd)", 2D) = "white" {}
+		[NoScaleOffset] _UpTex2("Up [+Y] (2:nd)", 2D) = "white" {}
+		[NoScaleOffset] _DownTex2("Down [-Y] (2:nd)", 2D) = "white" {}
+		_Tint2("Tint Color (2:nd)", Color) = (.5, .5, .5, .5)
+		[Gamma] _Exposure2("Exposure (2:nd)", Range(0, 8)) = 1.0
+	}
+
+	SubShader
+	{
+		Tags{ "Queue" = "Background" "RenderType" = "Background" }
+		Cull Off
+		ZWrite Off
+		Fog{ Mode Off }
+		Lighting Off
+		//Color[_Tint] what is dis??
+
+		CGINCLUDE
+		#include "UnityCG.cginc"
+
+		half4 _Tint1;
+		half _Exposure1;
+		half4 _Tint2;
+		half _Exposure2;
+		
+		float _Rotation;
+		half _Blend;
+
+		float4 RotateAroundYInDegrees(float4 vertex, float degrees)
+		{
+			float alpha = degrees * UNITY_PI / 180.0;
+			float sina, cosa;
+			sincos(alpha, sina, cosa);
+			float2x2 m = float2x2(cosa, -sina, sina, cosa);
+			return float4(mul(m, vertex.xz), vertex.yw).xzyw;
+		}
+
+		struct appdata_t {
+			float4 vertex : POSITION;
+			float2 texcoord : TEXCOORD0;
+		};
+		struct v2f {
+			float4 vertex : SV_POSITION;
+			float2 texcoord : TEXCOORD0;
+		};
+		v2f vert(appdata_t v)
+		{
+			v2f o;
+			o.vertex = mul(UNITY_MATRIX_MVP, RotateAroundYInDegrees(v.vertex, _Rotation));
+			o.texcoord = v.texcoord;
+			return o;
+		}
+		//Old
+		/*
+		half4 skybox_frag(v2f i, sampler2D smp, half4 smpDecode)
+		{
+			half4 tex = tex2D(smp, i.texcoord);
+			half3 c = DecodeHDR(tex, smpDecode);
+			c = c * _Tint.rgb * unity_ColorSpaceDouble.rgb;
+			c *= _Exposure;
+			return half4(c, 1);
+		}*/
+		//Rewritten:
+		/*half4 skybox_frag(v2f i, sampler2D smp, sampler2D smp2)
+		{
+			//half4 tex = tex2D(smp, i.texcoord) * tex2D(smp2, i.texcoord); ACTUALLY BLENDS!!!
+			half4 tex = tex2D(smp, i.texcoord) * half4(1, 1, 1, (1 - _Blend));
+			half4 tex2 = tex2D(smp2, i.texcoord) * half4(1, 1, 1, _Blend);
+
+			half3 c = half3(tex.x, tex.y, tex.z);
+			half3 c2 = half3(tex2.x, tex2.y, tex2.z);
+
+			half3 d = c * c2;
+
+			d = d * _Tint.rgb * unity_ColorSpaceDouble.rgb;
+			d *= _Exposure;
+			return half4(d, 1);
+		}*/
+		//No consideration to exposure or tint
+		fixed4 skybox_frag(v2f i, sampler2D smp1, sampler2D smp2)
+		{
+			//TEST (WORKS!)
+			half4 tex1 = tex2D(smp1, i.texcoord);
+			half3 c = half3(tex1.x, tex1.y, tex1.z);
+			c = c * _Tint1.rgb * unity_ColorSpaceDouble.rgb;
+			c *= _Exposure1;
+
+			//Could make two separate tints and exposures...
+			//For tex2 here obviously
+			half4 tex2 = tex2D(smp2, i.texcoord);
+			half3 c2 = half3(tex2.x, tex2.y, tex2.z);
+			c2 = c2 * _Tint2.rgb * unity_ColorSpaceDouble.rgb;
+			c2 *= _Exposure2;
+
+			fixed4 texFinal = lerp(half4(c, 1), half4(c2, 1), _Blend);
+			return texFinal;
+		}
+		ENDCG
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _FrontTex;
+			sampler2D _FrontTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _FrontTex, _FrontTex2); }
+			ENDCG
+			/*CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _FrontTex;
+			sampler2D _FrontTex2;
+			half4 frag(v2f i) : SV_Target{ return skybox_frag(i, _FrontTex, _FrontTex2); }
+			ENDCG*/
+			//SetTexture[_FrontTex]{ combine texture }
+			//SetTexture[_FrontTex2]{ constantColor(0,0,0,[_Blend]) combine texture lerp(constant) previous }
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _BackTex;
+			sampler2D _BackTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _BackTex, _BackTex2); }
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _LeftTex;
+			sampler2D _LeftTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _LeftTex, _LeftTex2); }
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _RightTex;
+			sampler2D _RightTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _RightTex, _RightTex2); }
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _UpTex;
+			sampler2D _UpTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _UpTex, _UpTex2); }
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			sampler2D _DownTex;
+			sampler2D _DownTex2;
+			fixed4 frag(v2f i) : SV_Target{ return skybox_frag(i, _DownTex, _DownTex2); }
+			ENDCG
+		}
+	}
+
+	Fallback "Skybox/6 Sided", 1
+}
